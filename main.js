a5a216f0eddb6cb85713282607802f7b81720691
MATSUMOTO-Takashi <t.matsumoto0308@gmail.com>
2015-01-21 03:32:45 +0900

    /f の任意化

--- main.js
+++ main.js
@@ -5,30 +5,29 @@ if (nparams.Exists('h')) usage();
 if (params.Count != 1) usage();
 
 var fileSysObj = new ActiveXObject('Scripting.FileSystemObject');
-var scriptPath = String(WScript.ScriptFullName).replace(WScript.ScriptName,'');
+var scriptPath = String(WScript.ScriptFullName).replace(WScript.ScriptName, '');
 
 var configName = 'config.json';
 if (nparams.Exists('f')) {
   configName = nparams.Item('f');
 }
 
-var configPath = fileSysObj.BuildPath(scriptPath, configName);
-
 var outputFileName = 'output.pptx';
 if (nparams.Exists('o')) {
   outputFileName = nparams.Item('o');
 }
 
-// copy src to dst
-var src = fileSysObj.BuildPath(scriptPath, params.Item(0));
-var dst = fileSysObj.BuildPath(scriptPath, outputFileName);
+var configPath = fileSysObj.BuildPath(scriptPath, configName);
+var srcPath    = fileSysObj.BuildPath(scriptPath, params.Item(0));
+var dstPath    = fileSysObj.BuildPath(scriptPath, outputFileName);
 
-fileSysObj.CopyFile(src, dst);
+fileSysObj.CopyFile(srcPath, dstPath);
 
 
 // load config file
 var configFile = fileSysObj.OpenTextFile(configPath, 1, false, -2);
 var config = eval('(' + configFile.ReadAll() + ')');
+// descending sort
 config.del.sort(function(a, b) {
   return b - a;
 });
@@ -36,7 +35,8 @@ config.del.sort(function(a, b) {
 
 // edit powerpoint file
 var pptObj = WScript.CreateObject('PowerPoint.Application');
-pptObj.Presentations.Open(dst);
+pptObj.Presentations.Open(dstPath);
+
 for (var i = 0; i < config.del.length; i++) {
   pptObj.ActivePresentation.Slides(config.del[i]).Delete();
 }
@@ -46,10 +46,11 @@ pptObj.Quit();
 
 
 function usage() {
-  var msg = 'main.bat /f [/o] 元ファイル\n' +
+  var msg = 'main.bat [/f] [/o] 元ファイル\n' +
             '  元ファイル  変換元のファイル\n' +
-            '  /f  設定ファイル\n' +
-            '  /o  出力ファイル名\n';
+            '  /f  設定ファイル (default:config.json)\n' +
+            '  /o  出力ファイル名 (default:output.pptx)\n' +
+            '  /h  ヘルプ\n';
   WScript.Echo(msg);
   WScript.Quit();
 }
